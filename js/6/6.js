fb3ef62a98fffac308e9c0755c96b2dba0f0ca88
Your Name <you@example.com>
2024-08-22 10:51:21 +0530

    js6

--- js/6/6.js
+++ js/6/6.js
@@ -2,20 +2,27 @@ function check(){
     let email=document.getElementById("mail").value
     let [username,rest]=email.split("@")
 
-    if(rest.includes(".") && username!=null){
+    if(email.includes("@"))
+    {
+        if(rest.includes(".") && username!=null){
     
-        let [server,domain]=rest.split(".")
-        if(domain.length>=2 && server.length>=1)
-        {
-         document.getElementById("valid").innerHTML="Valid Emailid"
+            let [server,domain]=rest.split(".")
+            if(domain.length>=2 && server.length>=1)
+            {
+             document.getElementById("valid").innerHTML="Valid Emailid"
+            }
+            else{
+            document.getElementById("valid").innerHTML="Not a valid Emailid"
+            }
         }
-        else{
-        document.getElementById("valid").innerHTML="Not a valid Emailid"
+        else
+        {
+             document.getElementById("valid").innerHTML="Not a valid Emailid"
         }
     }
     else
-    {
-         document.getElementById("valid").innerHTML="Not a valid Emailid"
-    }
+        {
+             document.getElementById("valid").innerHTML="Not a valid Emailid"
+        }
 
 }
